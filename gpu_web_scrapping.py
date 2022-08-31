fe4376d31a2bb1973af68c7cc950fb28b10f7ea9
Jose Armas <armasjose606@gmail.com>
2022-08-30 22:11:00 -0400

    Add files via upload

---
+++ gpu_web_scrapping.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+import requests
+
+def check_price(no_pages):
+
+    url = 'https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7709&PageSize=96'
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"}
+    page = requests.get(url, headers=headers)
+    
+    soup1 = BeautifulSoup(page.content, 'html.parser')
+    soup2 = BeautifulSoup(soup1.prettify(), 'html.parser')
+    
+    gfxcards = []
+    
+    import datetime
+    
+    for list in soup2.find_all('div', class_='item-container'):
+        title = list.find('a', class_='item-title').get_text().replace('\n', '').replace('RADEON', 'Radeon').strip()
+        price = list.find('li', class_='price-current').get_text().replace('\n', '').strip().split()
+        link = list.find('a')['href']
+        shipping = list.find('li', class_='price-ship').get_text().replace('\n', '').strip()
+        
+        allCards = []
+        
+        if "GeForce" in title:
+            allCards.append("Nvidia")
+        elif "Radeon" in title:
+            allCards.append("Radeon")
+        else:
+            allCards.append("Unknown")
+        
+        allCards.append(title.split()[0])
+        
+        if "GeForce" in title and 'Ti' not in title:
+            allCards.append(title.split("GeForce",1)[1][1:9])
+        elif "GeForce" and 'Ti' in title:
+            allCards.append(title.split("GeForce",1)[1][1:12])
+        elif "Radeon" and 'XT' in title:
+            allCards.append(title.split("Radeon",1)[-1][1:11])
+        elif 'Radeon' in title and 'XT' not in title:
+            allCards.append(title.split("Radeon",1)[1][1:8])
+        else:
+            allCards.append('Unknown')
+
+        try:
+            allCards.append(int(price[1]))
+        except:
+            allCards.append(0)
+
+        allCards.append(link)
+        
+        allCards.append(shipping)
+        
+        gfxcards.append(allCards)
+
+    return gfxcards
---
+++ main.py
@@ -0,0 +1,14 @@
+from csv import writer
+import pandas as pd
+from gpu_web_scrapping import *
+1
+no_pages = int(input('how many pages do you want to scrape?: '))
+
+results = []
+for i in range(1, no_pages+1): 
+    results.append(check_price(i))
+
+unpack = lambda l: [item for sublist in l for item in sublist]
+
+df = pd.DataFrame(unpack(results),columns=['Manufacturer','Brand','Model','Price', 'link', 'Shipping'])
+df.to_csv('Graphics card price.csv', index=False, encoding='utf-8')
